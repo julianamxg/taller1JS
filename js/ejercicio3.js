c53a61f72fec5398396bee5c2ab58be60711f2a7
julianamxg <jamelo827@misena.edu.co>
2022-09-08 22:06:56 -0500

    cuarto commit - ejercicio 3

---
+++ js/ejercicio3.js
@@ -0,0 +1,21 @@
+const calcular = document.getElementById('calcular');
+
+calcular.addEventListener('click', function(){
+    const primerLado = document.getElementById('primerLado').value;
+    const segundoLado = document.getElementById('segundoLado').value;
+    const tercerLado = document.getElementById('tercerLado').value;
+
+    parseInt(primerLado, segundoLado, tercerLado);
+   
+    if(primerLado == segundoLado && primerLado == tercerLado && segundoLado == tercerLado){       
+        alert(`El triangulo es equilatero`);
+    }
+
+    else if(primerLado == segundoLado || primerLado == tercerLado ||segundoLado == tercerLado){       
+        alert(`El triangulo es isosceles`);
+    }
+
+    else if(primerLado != segundoLado && primerLado != tercerLado ||segundoLado != tercerLado){       
+        alert(`El triangulo es escaleno`);
+    }
+})
