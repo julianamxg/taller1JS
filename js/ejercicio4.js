df401d9fa4665d617611d5fe9b5ed88d27a1bae3
julianamxg <jamelo827@misena.edu.co>
2022-09-08 22:52:50 -0500

    quinto commit - ejercicio 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,172 @@
+const consultar = document.getElementById('consultar');
+
+consultar.addEventListener('click', function () {
+    const opcionN = document.getElementById('opcionN').value;
+    const opcionA = document.getElementById('opcionA').value;
+    const opcionAc = document.getElementById('opcionAc').value;
+    parseInt(opcionN, opcionA, opcionAc);
+
+    //Profe tatiana
+        //Respuesta correcta
+        if (opcionN == 1 && opcionA == 6 && opcionAc == 11) {
+        alert(`Respuesta correcta`);
+        }
+
+        //Respuesta incorrecta
+        else if (opcionN != 1 && opcionA != 6 && opcionAc == 11) {
+        alert(`Respuesta incorrecta`);
+        }
+
+        else if (opcionN == 1 && opcionA != 6 && opcionAc != 11) {
+            alert(`Respuesta incorrecta`);
+        }
+
+        else if (opcionN == 1 && opcionA != 6 && opcionAc == 11) {
+            alert(`Respuesta incorrecta`);
+        }
+
+        else if (opcionN == 1 && opcionA == 6 && opcionAc != 11) {
+            alert(`Respuesta incorrecta`);
+        }
+
+        else if (opcionN == 1 && opcionA == 6 && opcionAc != 11) {
+            alert(`Respuesta incorrecta`);
+        }
+
+        else if (opcionN != 1 && opcionA == 6 && opcionAc == 11) {
+            alert(`Respuesta incorrecta`);
+        }
+    
+
+
+    //Profe Carolina
+        //Respuesta correcta
+        else if (opcionN == 2 && opcionA == 7 && opcionAc == 12) {
+        alert('Respuesta correcta')
+        }
+
+        //Respuesta incorrecta
+        else if (opcionN != 2 && opcionA != 7 && opcionAc == 12) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 2 && opcionA != 7 && opcionAc != 12) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN != 2 && opcionA == 7 && opcionAc != 12) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 2 && opcionA == 7 && opcionAc != 12) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN != 2 && opcionA == 7 && opcionAc == 12) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 2 && opcionA != 7 && opcionAc == 12) {
+            alert('Respuesta incorrecta')
+        }
+
+        
+    //Profe yaneth
+        //Respuesta correcta
+        else if (opcionN == 3 && opcionA == 8 && opcionAc == 13) {
+            alert('Respuesta correcta')
+        }
+
+        //Respuestas incorrectas
+        else if (opcionN != 3 && opcionA != 8 && opcionAc == 13) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 3 && opcionA != 8 && opcionAc != 13) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN !== 3 && opcionA == 8 && opcionAc != 13) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 3 && opcionA == 8 && opcionAc != 13) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN != 3 && opcionA == 8 && opcionAc == 13) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 3 && opcionA != 8 && opcionAc == 13) {
+            alert('Respuesta incorrecta')
+        }
+
+    //Profe fernando
+        //Respuesta correcta
+        else if (opcionN == 4 && opcionA == 9 && opcionAc == 14) {
+            alert('Respuesta correcta')
+        }
+
+        //Respuestas incorrectas
+        else if (opcionN != 4 && opcionA != 9 && opcionAc == 14) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 4 && opcionA != 9 && opcionAc != 14) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN != 4 && opcionA == 9 && opcionAc != 14) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 4 && opcionA == 9 && opcionAc != 14) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN != 4 && opcionA == 9 && opcionAc == 14) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 4 && opcionA != 9 && opcionAc == 14) {
+            alert('Respuesta incorrecta')
+        }
+
+
+    //Profe Cristian  
+        //Respuesta correcta
+        else if (opcionN == 5 && opcionA == 10 && opcionAc == 15) {
+            alert('Respuesta correcta')
+        }
+
+        //Respuestas incorrectas
+        else if (opcionN != 5 && opcionA != 10 && opcionAc == 15) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 5 && opcionA != 10 && opcionAc != 15) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN != 5 && opcionA == 10 && opcionAc != 15) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 5 && opcionA == 10 && opcionAc != 15) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN != 5 && opcionA == 10 && opcionAc == 15) {
+            alert('Respuesta incorrecta')
+        }
+
+        else if (opcionN == 5 && opcionA != 10 && opcionAc == 15) {
+            alert('Respuesta incorrecta')
+        }
+
+        
+
+
+
+})
